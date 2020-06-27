bb27e0e62dce4de67571e92656c7c0a92c0b434d
hassanyakef <yhassan@vt.edu>
2020-06-27 17:43:08 -0400

    Add "REACT_APP" prefix to env variables

--- src/app/config/keys_prod.js
+++ src/app/config/keys_prod.js
@@ -1,9 +1,9 @@
 module.exports = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: "eureka-v2",
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 }
